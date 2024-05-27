0e1c38df62cfac9143fbdf9d7cebfcf389de457e
PROJECTHUBSRM <ksat70080@gmail.com>
2024-05-27 23:07:14 +0530

    Update Main.tf

--- JENKINS-TF/Main.tf
+++ JENKINS-TF/Main.tf
@@ -1,13 +1,13 @@
-resource "aws_security_group" "Jenkins-sg" {
-  name        = "Jenkins-Security Group"
-  description = "Open 22,443,80,8080,9000"
+resource "aws_security_group" "jenkins_sg" {
+  name        = "Jenkins-Security-Group"
+  description = "Open 22, 80, 443, 8080, 9000, 3000"
 
-  # Define a single ingress rule to allow traffic on all specified ports
-  ingress = [
-    for port in [22, 80, 443, 8080, 9000, 3000] : {
-      description      = "TLS from VPC"
-      from_port        = port
-      to_port          = port
+  dynamic "ingress" {
+    for_each = [22, 80, 443, 8080, 9000, 3000]
+    content {
+      description      = "Allow traffic on port ${ingress.value}"
+      from_port        = ingress.value
+      to_port          = ingress.value
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = []
@@ -15,7 +15,7 @@ resource "aws_security_group" "Jenkins-sg" {
       security_groups  = []
       self             = false
     }
-  ]
+  }
 
   egress {
     from_port   = 0
@@ -29,17 +29,17 @@ resource "aws_security_group" "Jenkins-sg" {
   }
 }
 
-
 resource "aws_instance" "web" {
   ami                    = "ami-0f403e3180720dd7e"
   instance_type          = "t2.large"
   key_name               = "Amazon-app-key"
-  vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
-  user_data              = templatefile("./install_jenkins.sh", {})
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+  user_data              = file("./install_jenkins.sh")
 
   tags = {
     Name = "Jenkins-sonar"
   }
+
   root_block_device {
     volume_size = 30
   }
